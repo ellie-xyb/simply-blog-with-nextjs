1d99c3548bbef5f6ad6443b2308d4fca9298cb7c
Ellie Xu <47287801+ellie-xyb@users.noreply.github.com>
2022-03-18 21:28:21 +0900

    pass post content into props and send to page

--- pages/index.js
+++ pages/index.js
@@ -20,9 +20,6 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <Link href="/posts/id">
-          <a>Go to posts</a>
-        </Link>
         <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
           <h2 className={utilStyles.headingLg}>Blog</h2>
           <ul className={utilStyles.list}>
